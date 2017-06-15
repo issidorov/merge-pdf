6e07044b015263e0ba361a2a56cc88d94f57c6ae
Ivan Sidorov <iv.serg.sidorov@gmail.com>
2017-06-15 14:44:25 +0300

    Fix bugs

--- tests/MainTest.php
+++ tests/MainTest.php
@@ -1,7 +1,7 @@
 <?php
 
 require __DIR__ . '/../vendor/autoload.php';
-require __DIR__ . '/../MergePdf.php';
+require __DIR__ . '/../src/MergePdf.php';
 
 use issidorov\mergePdf\MergePdf;
 use PHPUnit\Framework\TestCase;
